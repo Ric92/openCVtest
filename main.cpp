8158428db867e9e676f3f4d7c72c11abc16b0723
Unknown <ricloplop@gmail.com>
2018-03-28 19:25:58 +0200

    Pinhole model + display

--- main.cpp
+++ main.cpp
@@ -6,9 +6,40 @@ using namespace cv;
 using namespace std;
 
 int main(int argc, char** argv )
-{
-    std::vector<cv::Point3f> objectPoints={cv::Point3f(1,0,5),cv::Point3f(2,2,5),cv::Point3f(3,0,10),cv::Point3f(4,-2,10),cv::Point3f(5,0,10)};
-    Mat M(2,2, CV_8UC3, Scalar(0,0,255));
-    cout << "M = " << endl << " " << objectPoints << endl;
+{    
+    cout << "Points" << endl;
+
+    vector<cv::Point3f> objectPoints={Point3f(1,3,5),Point3f(2,2,5),Point3f(3,0,10),Point3f(4,-2,10),Point3f(5,0,10)};
+    cout << objectPoints << endl << endl;
+
+    cout << "Intrinsic" << endl << endl;
+
+    Mat intrinsic=(cv::Mat1d(3, 3) << 300, 0, 320, 0, 300, 240, 0, 0, 1);
+    cout << intrinsic << endl << endl;
+
+    cv::Mat translationVector=(cv::Mat_<double>(1, 3) << 0 ,0 ,0);
+    cout << "Translation vector: "<< translationVector << endl << endl;
+
+    cout << "Rotation matrix" << std::endl;
+    Mat opencvRotMatrix=(cv::Mat_<double>(3,3) <<
+                             1,0,0,
+                             0,1,0,
+                             0,0,1);
+    cout << opencvRotMatrix << endl << endl;
+    Mat rotationVector;
+    Rodrigues(opencvRotMatrix,rotationVector);
+    cout << "Rotation vector associated: " << rotationVector <<endl<<endl;
+    std::vector<cv::Point2f> projections;
+    projectPoints(objectPoints,rotationVector,translationVector,intrinsic,cv::noArray(),projections,cv::noArray(),0);
+    cout << "Projected points" << endl << projections <<endl;
+    cv::Mat display = cv::Mat::zeros(480,640,CV_8UC3);
+    int j=1;
+    for(auto &feature: projections){
+                std::cout << feature.x << ", " << feature.y << std::endl;
+                cv::circle(display, feature, 3, cv::Scalar(255,0,0),3);
+                j++;
+            }
+    cv::imshow("Camara 1", display);
+    cv::waitKey();
     return 0;
 }
